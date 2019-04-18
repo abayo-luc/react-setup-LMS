585f2635281cc7c990f8f188997c5f605b559ba0
Luc <luc.bayo@gmail.com>
2019-04-18 17:48:02 +0200

    Initial commit

---
+++ __tests__/containers/Home.spec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import Home from '../../src/containers/Home';
+describe('Home container', () => {
+  it('should match the snapshot', () => {
+    const home = shallow(<Home />);
+    expect(toJson(home)).toMatchSnapshot();
+  });
+});
